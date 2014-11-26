bd836a244798c388a67b14cbb5d58bb789621a18
Kittiphong Xayasane <kxayasan@gmail.com>
2014-11-26 13:35:25 -0500

    added item class

---
+++ fuck/Item.h
@@ -0,0 +1,18 @@
+//
+//  Item.h
+//  fuck
+//
+//  Created by kittiphong xayasane on 2014-11-26.
+//  Copyright (c) 2014 Kittiphong Xayasane. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Item : NSObject
+
+@property (nonatomic, strong) NSString * name;
+
+-(id)initWithName:(NSString *)name;
+
+
+@end
---
+++ fuck/Item.m
@@ -0,0 +1,21 @@
+//
+//  Item.m
+//  fuck
+//
+//  Created by kittiphong xayasane on 2014-11-26.
+//  Copyright (c) 2014 Kittiphong Xayasane. All rights reserved.
+//
+
+#import "Item.h"
+
+@implementation Item
+
+-(id)initWithName:(NSString *)name {
+    self = [super init];
+    
+    self.name = name;
+    
+    return self;
+}
+
+@end
